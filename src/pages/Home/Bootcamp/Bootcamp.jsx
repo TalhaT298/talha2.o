fdaa83e09d58bc406d6e330515f5862b0b3d2a6d
talha <mdtalha2008@gmail.com>
2025-01-11 02:38:45 +0600

    bootcamp slider add and bootcamp add at home.jsx

--- src/pages/Home/Bootcamp/Bootcamp.jsx
+++ src/pages/Home/Bootcamp/Bootcamp.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import BootcampSlider from './BootcampSlider';
 const Bootcamp = () => {
   return (
-    <div>
-      
+    <div className="lg:hidden block">
+    <div className='max-w-[1400px] mx-auto py-12 lg:p-10' data-aos="">
+        <div className="flex flex-wrap justify-around text-white p-3">
+            <div className=''>
+            {/* <h1 className='text-3xl mb-12 mt-20 font-bold text-center'>Services</h1> */}
+            </div>
+            {/* <div className="flex flex-wrap justify-center">
+            
+            <h6 className='md:mx-6 md:mt-6 pl-6 md:pl-0 '>Intensive training sessions to dominate the competition</h6>
+        </div> */}
+        </div>
+
+        <div className="slider">
+        <h1 className='text-3xl mb-12 mt-20 font-bold text-center'>Services</h1>
+        <BootcampSlider />
+        </div>
+    </div>
+    {/* <img className=' mx-auto' src={line} alt="" />   */}
     </div>
   )
 }
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,12 +4,14 @@ import Github from '../Github/Github';
 import Earth from '../Earth/Earth';
 import CopyToClipboard from '../CopyToClipboard/CopyToClipboard';
 import Card from '../Card/Card';
+import Bootcamp from '../Bootcamp/Bootcamp';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Card></Card>
+            <Bootcamp></Bootcamp>
             <Earth></Earth>
             <Github></Github>
             <CopyToClipboard></CopyToClipboard>
