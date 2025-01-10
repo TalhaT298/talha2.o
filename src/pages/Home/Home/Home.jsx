de11bdbf46335f69dad17f03f9e4fdc15ac22344
talha <mdtalha2008@gmail.com>
2025-01-11 02:09:40 +0600

    card add perfectly

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,11 +3,13 @@ import Banner from '../Banner/Banner';
 import Github from '../Github/Github';
 import Earth from '../Earth/Earth';
 import CopyToClipboard from '../CopyToClipboard/CopyToClipboard';
+import Card from '../Card/Card';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Card></Card>
             <Earth></Earth>
             <Github></Github>
             <CopyToClipboard></CopyToClipboard>
