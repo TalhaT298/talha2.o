bb09f84153fbe166394b6165905e2f77f9ad1b05
talha <mdtalha2008@gmail.com>
2025-01-11 02:48:40 +0600

    banner done

--- src/pages/Home/Bootcamp/Bootcamp.jsx
+++ src/pages/Home/Bootcamp/Bootcamp.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import BootcampSlider from './BootcampSlider';
+import React from "react";
+import BootcampSlider from "./BootcampSlider";
 const Bootcamp = () => {
   return (
     <div className="lg:hidden block">
-    <div className='max-w-[1400px] mx-auto py-12 lg:p-10' data-aos="">
+      <div className="max-w-[1400px] mx-auto py-12 lg:p-10" data-aos="">
         <div className="flex flex-wrap justify-around text-white p-3">
-            <div className=''>
-            {/* <h1 className='text-3xl mb-12 mt-20 font-bold text-center'>Services</h1> */}
-            </div>
-            {/* <div className="flex flex-wrap justify-center">
-            
-            <h6 className='md:mx-6 md:mt-6 pl-6 md:pl-0 '>Intensive training sessions to dominate the competition</h6>
-        </div> */}
+          <div className=""></div>
         </div>
 
         <div className="slider">
-        <h1 className='text-3xl mb-12 mt-20 font-bold text-center'>Services</h1>
-        <BootcampSlider />
+          <h1 className="text-3xl mb-12 mt-20 font-bold text-center">
+            Services
+          </h1>
+          <BootcampSlider />
         </div>
+      </div>
     </div>
-    {/* <img className=' mx-auto' src={line} alt="" />   */}
-    </div>
-  )
-}
+  );
+};
 
-export default Bootcamp
+export default Bootcamp;
--- src/pages/Home/Bootcamp/BootcampSlider.jsx
+++ src/pages/Home/Bootcamp/BootcampSlider.jsx
@@ -1,10 +1,120 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+// import { Keyboard, Scrollbar, Navigation, Pagination } from "swiper";
+import { Keyboard, Scrollbar, Navigation, Pagination } from 'swiper/modules';
+import img1 from '../../../assets/image 90.png'
+import img2 from '../../../assets/WhatsApp Image 2023-04-20 at 11.49 1.png'
+import img3 from '../../../assets/image 92.png'
+import img4 from '../../../assets/Vector 10.png'
 
 const BootcampSlider = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='py-8 max-w-[1700px]  mx-auto'>
+        <Swiper
+        slidesPerView={1}
+        centeredSlides={false}
+        slidesPerGroupSkip={1}
+        grabCursor={true}
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1300: {
+            slidesPerView: 3,
+          },
+          
+
+        }}
+        //scrollbar={true}
+        navigation={true}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        modules={[Keyboard, Scrollbar, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+        <div className="mx-auto w-[400px] rounded-2xl group relative items-center justify-center overflow-hidden cursor-pointer">         
+          <div className="">
+            <span className="top-96 mt-4 mx-8 flex flex-wrap text-white absolute z-20">See Bootcamps
+            <img className='h-3 w-3 mx-2 mt-2' src={img4} alt="" />
+            </span>
+           <img className='w-[400px] relative  transition-transform duration-500' src={img1} alt="" />            
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent  group-hover:from-black/70  group-hover:to-black/70"></div>
+          <div className="absolute inset-0 flex flex-col items-center justify-center  text-center translate-y-[60%] group-hover:translate-y-0 transition-all duration-500">
+            <h1 className='text-white font-bold text-[20px] pr-3'>Full Stack <br />Development </h1>
+          </div>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="mx-auto w-[400px] rounded-2xl group relative items-center justify-center overflow-hidden cursor-pointer">         
+          <div className="">
+            <span className="top-96 mt-4 mx-8 flex flex-wrap text-white absolute z-20">See Bootcamps
+            <img className='h-3 w-3 mx-2 mt-2' src={img4} alt="" />
+            </span>
+           <img className='w-[400px] relative  transition-transform duration-500' src={img2} alt="" />            
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent  group-hover:from-black/70  group-hover:to-black/70"></div>
+          <div className="absolute inset-0 flex flex-col items-center justify-center  text-center translate-y-[60%] group-hover:translate-y-0 transition-all duration-500">
+            <h1 className='text-white font-bold text-[20px] pr-3'>MERN Stack <br />Development </h1>
+          </div>
+        </div>         
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="mx-auto w-[400px] rounded-2xl group relative items-center justify-center overflow-hidden cursor-pointer">         
+          <div className="">
+            <span className="top-96 mt-4 mx-8 flex flex-wrap text-white absolute z-20">See Bootcamps
+            <img className='h-3 w-3 mx-2 mt-2' src={img4} alt="" />
+            </span>
+           <img className='w-[400px] relative  transition-transform duration-500' src={img3} alt="" />            
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent  group-hover:from-black/70  group-hover:to-black/70"></div>
+          <div className="absolute inset-0 flex flex-col items-center justify-center  text-center translate-y-[60%] group-hover:translate-y-0 transition-all duration-500">
+            <h1 className='text-white font-bold text-[20px] pr-3'>Frontend <br /> Development </h1>
+          </div>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="mx-auto w-[400px] rounded-2xl group relative items-center justify-center overflow-hidden cursor-pointer">         
+          <div className="">
+            <span className="top-96 mt-4 mx-8 flex flex-wrap text-white absolute z-20">See Bootcamps
+            <img className='h-3 w-3 mx-2 mt-2' src={img4} alt="" />
+            </span>
+           <img className='w-[400px] relative  transition-transform duration-500' src={img1} alt="" />            
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent  group-hover:from-black/70  group-hover:to-black/70"></div>
+          <div className="absolute inset-0 flex flex-col items-center justify-center  text-center translate-y-[60%] group-hover:translate-y-0 transition-all duration-500">
+            <h1 className='text-white font-bold text-[20px] pr-3'>Full Stack <br />Development</h1>
+          </div>
+        </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="mx-auto w-[400px] rounded-2xl group relative items-center justify-center overflow-hidden cursor-pointer">         
+          <div className="">
+            <span className="top-96 mt-4 mx-8 flex flex-wrap text-white absolute z-20">See Bootcamps
+            <img className='h-3 w-3 mx-2 mt-2' src={img4} alt="" />
+            </span>
+           <img className='w-[400px] relative  transition-transform duration-500' src={img2} alt="" />            
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent  group-hover:from-black/70  group-hover:to-black/70"></div>
+          <div className="absolute inset-0 flex flex-col items-center justify-center  text-center translate-y-[60%] group-hover:translate-y-0 transition-all duration-500">
+            <h1 className='text-white font-bold text-[20px] pr-3'>MERN Stack <br /> Development</h1>
+          </div>
+        </div>
+        </SwiperSlide>
+      </Swiper>
+        </div>
   )
 }
 
