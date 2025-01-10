fe0aad500925347c4d41aa4798b6c709ca9935d0
talha <mdtalha2008@gmail.com>
2025-01-11 00:41:45 +0600

    github add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Github from '../Github/Github';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Github></Github>
         </div>
     );
 };
