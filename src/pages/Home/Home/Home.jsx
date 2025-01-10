6bf1ea8e95f13591a96e8839b446bc2826821e2e
talha <mdtalha2008@gmail.com>
2025-01-11 01:00:36 +0600

    earth add at home.jsx

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Github from '../Github/Github';
+import Earth from '../Earth/Earth';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Github></Github>
+            <Earth></Earth>
         </div>
     );
 };
